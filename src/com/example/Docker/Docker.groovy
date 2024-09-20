87be65fa24d0549b406a6c351ac71623eaf43f72
Eugene Shmorgun <eugene.shmorgun@vmedia.ca>
2024-09-20 16:01:51 +0300

    new methods

--- src/com/example/Docker/Docker.groovy
+++ src/com/example/Docker/Docker.groovy
@@ -12,11 +12,28 @@ class Docker implements Serializable{
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image ..."
+        script.sh "docker build -t $imageName ."
+
+//        script.withCredentials([script.usernamePassword(credentialsId: 'fe0a4b7c-4e6b-48fb-8744-585d873a4b10', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//            script.sh "docker build -t $imageName ."
+//            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+//            script.sh "docker push $imageName"
+//        }
+    }
+
+    def dockerLogin() {
+        script.echo "building the docker image ..."
         script.withCredentials([script.usernamePassword(credentialsId: 'fe0a4b7c-4e6b-48fb-8744-585d873a4b10', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
         }
     }
+
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
+
+
 
 }
